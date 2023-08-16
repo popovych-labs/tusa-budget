b8cf3e5012f4a7b413c3d911d425ee9f8a197490
Oleksandr <ogvalt@gmail.com>
2023-08-16 20:44:41 +0300

    refactor(js): get for each page script separate function to improve readibility

--- backend/src/js/index.js
+++ backend/src/js/index.js
@@ -6,6 +6,68 @@ const path = currentUrl.pathname;
 
 const token = localStorage.getItem('token');
 
+function login_page() {
+    const form = document.getElementById("login");
+ 
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const errorMessage = document.getElementById("error-msg")
+
+        try {
+            const response = await fetch(`${domain}/api/token`, {
+                method: "POST",
+                body: formData,
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const bearerToken = data.access_token;
+                localStorage.setItem("token", bearerToken)
+
+                window.location.href = "/dashboard";
+                // Process the data on the frontend as needed.
+            } 
+            else if (response.status === 401) {
+                console.error("401 Unauthorized", response)
+                errorMessage.style.display = 'block';
+            }
+            else if (response.status === 404) {
+                console.error("404 Not Found", response)
+                errorMessage.style.display = "block";
+            }
+            else {
+                const errorData = await response.json();
+                console.error("Form submission error:", errorData);
+                // Handle the validation error on the frontend.
+            }
+        } catch (error) {
+            console.error("Server error:", error);
+            // Handle server-related errors on the frontend.
+        }
+    });
+};
+
+function dashboard_page(){
+    var button = document.getElementById("create_tusa");
+
+    button.addEventListener("click", () => {
+        window.location.href = "/tusa";
+    });
+    
+}
+
+function tusa_page(){
+    console.log("tusa")
+}
+
+var functionDict = {
+    "/login": login_page,
+    "/dashboard": dashboard_page,
+    "/tusa": tusa_page
+}
+
 fetch(currentUrl, {
     method: "POST",
     headers: {
@@ -25,55 +87,17 @@ fetch(currentUrl, {
         currentContent.innerHTML = newContent;
     }).
     then(() => {
-        if (path === "/login"){
-            const form = document.getElementById("login");
-         
-            form.addEventListener("submit", async (event) => {
-                event.preventDefault();
+        page_specific_script = functionDict[path];
         
-                const formData = new FormData(form);
-                const errorMessage = document.getElementById("error-msg")
-        
-                try {
-                    const response = await fetch(`${domain}/api/token`, {
-                        method: "POST",
-                        body: formData,
-                    });
-        
-                    if (response.ok) {
-                        const data = await response.json();
-                        const bearerToken = data.access_token;
-                        localStorage.setItem("token", bearerToken)
-        
-                        window.location.href = "/dashboard";
-                        // Process the data on the frontend as needed.
-                    } 
-                    else if (response.status === 401) {
-                        console.error("401 Unauthorized", response)
-                        errorMessage.style.display = 'block';
-                    }
-                    else if (response.status === 404) {
-                        console.error("404 Not Found", response)
-                        errorMessage.style.display = "block";
-                    }
-                    else {
-                        const errorData = await response.json();
-                        console.error("Form submission error:", errorData);
-                        // Handle the validation error on the frontend.
-                    }
-                } catch (error) {
-                    console.error("Server error:", error);
-                    // Handle server-related errors on the frontend.
-                }
-            });
+
+        if (page_specific_script === undefined){
+            throw new Error("Error in functionDict");
         }
-        else if (path === "/dashboard")
-        {
-            console.log("dashboard")
-        }
+
+        page_specific_script();
     })
     .catch(error => {
-        console.error('Error fetching data:', error);
+        console.error('Error caught:', error);
     });
 
 
